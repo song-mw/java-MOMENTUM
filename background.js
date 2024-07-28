bdb79ea1385c0a7874319eb45d78386709549a37
song-mw <myw.song@gmail.com>
2024-07-29 00:18:09 +0900

    Initial commit

---
+++ background.js
@@ -0,0 +1,7 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = `url(img/${chosenImage})`;
+
+document.body.style.backgroundImage = bgImage;
